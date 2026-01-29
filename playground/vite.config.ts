ce9facc8838c66e88e1de5b635320adc57bf846e
Miqayel <miqayelpetrosyan@gmail.com>
2026-01-29 18:31:54 +0400

    fix package

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,10 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+    base: './',
     plugins: [vue()],
     resolve: {
-        alias: {
-            '@vue-pagination/core': fileURLToPath(new URL('../src/index.ts', import.meta.url))
-        }
+        alias: [
+            { find: /^v-paginate-pretty$/, replacement: fileURLToPath(new URL('../src/index.ts', import.meta.url)) },
+            { find: /^v-paginate-pretty\/dist\/style\.css$/, replacement: fileURLToPath(new URL('../dist/style.css', import.meta.url)) }
+        ]
     }
 })
